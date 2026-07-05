09068656035f8db9bbbe50e3e1c3cc79fc56cfb2
SmartToolzAI <brazilmarketing1@gmail.com>
2026-07-05 12:29:08 -0700

    Inject Yandex Metrika counter analytics

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@
    ============================================ */
 
 // ==========================================
+// YANDEX METRIKA COUNTER (Analytics)
+// ==========================================
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110425579', 'ym');
+
+ym(110425579, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+
+// Append Yandex noscript fallback
+const yandexNoscript = document.createElement('noscript');
+yandexNoscript.innerHTML = '<div><img src="https://mc.yandex.ru/watch/110425579" style="position:absolute; left:-9999px;" alt="" /></div>';
+document.body.appendChild(yandexNoscript);
+
+// ==========================================
 // HEADER SCROLL EFFECT
 // ==========================================
 const header = document.getElementById('header');
